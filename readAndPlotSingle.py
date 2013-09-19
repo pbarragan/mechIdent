33b4a57f8085a0be2e6415fc24261adcf7721cb2
pbarragan <patrick.r.barragan@gmail.com>
2013-09-19 13:54:40 -0400

    Save after final ICRA 2014 submission and before adding connectivity to the robot

--- readAndPlotSingle.py
+++ readAndPlotSingle.py
@@ -39,19 +39,58 @@ def get_data(fileName):
     print len(data[0])
     return data, numSteps, numMPPairs, model
 
+def get_data_w_actions(fileName):
+    f = open(fileName,'r')
+    #read the initial stuff you need
+    f.readline()
+    model = int(f.readline())
+    skip_lines(f,5)
+    numStates = int(f.readline())
+    skip_lines(f,numStates*6+2)
+    numActions = int(f.readline())
+    skip_lines(f,numActions+2)
+    numMPPairs = int(f.readline())
+    skip_lines(f,numMPPairs*(6+1)+(1+1+1+1)+numStates)
+    data = []
+    for i in range(numMPPairs):
+        data.append([float(f.readline())])
+    skip_lines(f,3)
+    numSteps = int(f.readline())
+        
+    #this has to happen until you get to the end of the file
+    for i in range(numSteps):
+        skip_lines(f,5)
+        actionString = f.readline()
+        print actionString
+        skip_lines(f,5)
+        skip_lines(f,numStates+1+numMPPairs+1)
+        for j in range(numMPPairs):
+            data[j].append(float(f.readline()))
+    f.close()
+
+    #print model
+    #print numStates
+    #print numActions
+    #print numMPPairs
+    #print numSteps
+    #print len(data)
+    #print len(data[0])
+    return data, numSteps, numMPPairs, model
+
 #main
 #just to get the numbers we want
 model = 5
 asTypes = ['simple','random','entropy']
-asNum = 1
+asNum = 2
+trialNum = 1
 
 path = 'data/model'+str(model)+'/'+asTypes[asNum]+'/'
 
-startFileName = path+'data5_0.txt'
+startFileName = path+'data'+str(model)+'_'+str(trialNum)+'.txt'
 
 
 #setup
-startData, nSteps, nMPPairs, m = get_data(startFileName)
+startData, nSteps, nMPPairs, m = get_data_w_actions(startFileName)
 totalData = numpy.array(startData)
 
 inds = range(nSteps+1)
@@ -59,7 +98,7 @@ models = ['Free','Fixed','Rev','Pris','RevPrisL','PrisPrisL']
 for i in range(nMPPairs):
     pyplot.plot(inds,totalData[i,:],'-o')
 
-pyplot.title(models[m])
+pyplot.title(models[m]+' - '+asTypes[asNum])
 pyplot.ylabel('Probability')
 pyplot.xlabel('Step')
 pyplot.legend(models)
